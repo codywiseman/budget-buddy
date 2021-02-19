e5dc6a22cf6c979db8f555b7251840c611cd2e10
Cody Wiseman <cwiseman09@yahoo.com>
2021-02-19 00:29:05 +0000

    changed data

---
+++ database/data.sql
@@ -0,0 +1,2 @@
+insert into "users" ("firstName", "lastName", "email", "password", "accessToken")
+values ('budget', 'buddy', 'bb@gmail.com', 'BudgetBuddy!', 'access-sandbox-86e5e8a4-d6ff-47ff-ac0b-859d44b88ab7')
