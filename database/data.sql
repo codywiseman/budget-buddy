cf175f151d2b3bb0b08fc7605d56043729e79083
Cody Wiseman <cwiseman09@yahoo.com>
2021-03-29 21:18:36 -0700

    updated access-token for demo

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,2 @@
 insert into "users" ("firstName", "lastName", "email", "password", "accessToken")
-values ('budget', 'buddy', 'bb@gmail.com', 'BudgetBuddy!', 'access-sandbox-86e5e8a4-d6ff-47ff-ac0b-859d44b88ab7')
+values ('budget', 'buddy', 'bb@gmail.com', 'BudgetBuddy!', 'access-sandbox-396507ad-d237-4073-9347-b00e9f762f77')
